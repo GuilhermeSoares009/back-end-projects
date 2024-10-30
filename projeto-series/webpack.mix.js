c76a2c0b052ae844f4b4ce2efc310b509fe505d0
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-10-30 18:44:00 -0300

    Adicionando o breeze

--- projeto-series/webpack.mix.js
+++ projeto-series/webpack.mix.js
@@ -12,4 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix
-    .sass('resources/css/app.scss', 'public/css');
+    .js('resources/js/app.js','public/js')
+    .sass('resources/css/estilos.scss', 'public/css')
+    .postCss('resources/css/app.css', 'public/css',[
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]);
