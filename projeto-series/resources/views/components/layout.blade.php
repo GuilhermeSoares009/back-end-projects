5785735888aecb674bf9712fbcc9002754905e95
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-10-30 18:09:52 -0300

    Ajustando botões corretos

--- projeto-series/resources/views/components/layout.blade.php
+++ projeto-series/resources/views/components/layout.blade.php
@@ -14,8 +14,14 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a href="{{ route('series.index') }}" class="navbar-brand">Séries</a>
-
-            <a href="{{ route('logout') }}">Sair</a>
+            
+            @auth
+                <a href="{{ route('logout') }}">Sair</a>
+                @endauth
+                
+            @guest
+                <a href="{{ route('login') }}">Entrar</a>
+            @endguest
         </div>
     </nav>
 
--- projeto-series/resources/views/series/index.blade.php
+++ projeto-series/resources/views/series/index.blade.php
@@ -1,28 +1,32 @@
 <x-layout title="Séries" :mensagem-sucesso="$mensagemSucesso">
-
+    @auth
     <a href="{{ route('series.create') }}" class="btn btn-dark mb-2" >Adicionar</a>
-
+    @endauth
     <ul class="list-group">
         @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                <a href="{{ route('seasons.index', $serie->id) }}">
+                @auth<a href="{{ route('seasons.index', $serie->id) }}">@endauth
                     {{ $serie->nome }}
-                </a>
+                @auth</a>@endauth
 
-                <form action="{{ route('series.edit', $serie->id)  }}" method="get">
-                    @csrf
-                    <button class="btn btn-danger btn-sm">
-                        ✍
-                    </button>
-                </form>
-
-                <form action="{{ route('series.destroy', $serie->id)  }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger btn-sm">
-                        X
-                    </button>
-                </form>
+                @auth
+                    <span class="d-flex">
+                        <form action="{{ route('series.edit', $serie->id)  }}" method="get">
+                            @csrf
+                            <button class="btn btn-danger btn-sm">
+                                ✍
+                            </button>
+                        </form>
+        
+                        <form action="{{ route('series.destroy', $serie->id)  }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-sm">
+                                X
+                            </button>
+                        </form>
+                    </span>
+                @endauth
 
 
             </li>
