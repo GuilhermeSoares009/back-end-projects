fb591ff73fcdd8f028953b7b2bdf2878eaf55c44
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-11-03 19:16:59 -0300

    Exibindo a capa

--- projeto-series/resources/views/series/index.blade.php
+++ projeto-series/resources/views/series/index.blade.php
@@ -1,10 +1,21 @@
 <x-layout title="Séries" :mensagem-sucesso="$mensagemSucesso">
+
     @auth
     <a href="{{ route('series.create') }}" class="btn btn-dark mb-2" >Adicionar</a>
     @endauth
+    
     <ul class="list-group">
         @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">
+
+                <div class="d-flex align-items-center">
+                    <img src="{{ asset('storage/'.  ($serie->cover ?? 'series_cover/376467.png')) }}" 
+                         alt="Imagem da série" 
+                         width="100"
+                         class="img-thumbnail me-3">
+                </div>
+
+
                 @auth<a href="{{ route('seasons.index', $serie->id) }}">@endauth
                     {{ $serie->nome }}
                 @auth</a>@endauth
