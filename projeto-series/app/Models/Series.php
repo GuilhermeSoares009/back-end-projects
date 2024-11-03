39dfcfc44ed0ae1e2a69d60431d24d4362b57926
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-11-03 18:36:50 -0300

    Solicitando uma imagem

--- projeto-series/app/Models/Series.php
+++ projeto-series/app/Models/Series.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Builder;
 class Series extends Model
 {
     use HasFactory;
-    protected $fillable = ['nome'];
+    protected $fillable = ['nome','cover'];
 
     public function seasons()
     {
