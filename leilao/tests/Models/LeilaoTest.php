a2b7ccda1a4987c756685586ab2ff34d70f8beb1
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-12-03 14:47:05 -0300

    feat: escrevendo testes antes + adicionando test para lances repetidos

--- leilao/tests/Models/LeilaoTest.php
+++ leilao/tests/Models/LeilaoTest.php
@@ -11,6 +11,19 @@ use PHPUnit\Framework\TestCase;
 class LeilaoTest extends TestCase
 {
     
+    public function testLeilaoDeveReceberLancesRepetidos()
+    {
+        $leilao = new Leilao('Variante');
+        $ana = new Usuario('Ana');
+
+        $leilao->recebeLance(new Lance($ana, 1000));
+        $leilao->recebeLance(new Lance($ana, 1500));
+
+        static::assertCount(1, $leilao->getLances());
+        static::assertEquals(1000, $leilao->getLances()[0]->getValor());
+    }
+
+
     /**
      * @dataProvider geraLances
      */
